28ac5f47f545434351eb04b0516808da35154e22
Tania <mariataniaprete@icloud.com>
2024-12-01 16:09:21 +0100

    Routes + Controller

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,9 @@
 <?php
 
+use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/',[PageController::class,'welcome'])->name('welcome');
+
 
 
